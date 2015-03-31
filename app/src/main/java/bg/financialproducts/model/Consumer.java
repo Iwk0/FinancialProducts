f120947810ab7d6342349f589bed1ef59e50540e
Iwk0 <ivomishev@gmail.com>
2015-03-31 14:13:41 +0300

    Update

---
+++ app/src/main/java/bg/financialproducts/model/Consumer.java
@@ -0,0 +1,12 @@
+package bg.financialproducts.model;
+
+public class Consumer {
+
+    public String product;
+    public String apr;
+    public String currency;
+    public String monthlyPayment;
+    public String totalPayed;
+    public String interestRateType;
+    public String bank;
+}
